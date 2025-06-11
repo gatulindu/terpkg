feb0653d5cd2484b13aba1813c27aa819a38d409
gatulindu <gatulindu@keemail.me>
2025-06-11 00:39:16 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,24 @@
 #include "terpkg.h"
 #include <string.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	if (argc > 1 && strcmp(argv[1], "--init") == 0) {
-		init();
-	} else if (argc > 1 && strcmp(argv[1], "--run") == 0) {
-		run();
-	}	else if (argc > 1 && strcmp(argv[1], "--help") == 0) {
-		help();
-	} else {
-		help();
-	}
-	return 0;
+    if (argc <= 1) {
+        help();
+        return 0;
+    }
+
+    if (strcmp(argv[1], "--init") == 0) {
+        init();
+    } else if (strcmp(argv[1], "--run") == 0) {
+        run();
+    } else if (strcmp(argv[1], "--help") == 0) {
+        help();
+    } else {
+        fprintf(stderr, "Invalid argument: %s\n\n", argv[1]);
+        help();
+        return 1;
+    }
+
+    return 0;
 }
